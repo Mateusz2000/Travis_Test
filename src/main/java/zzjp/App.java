23dce7333532cac81a905041a36d3da6cd57556d
210165 Dominiak Mateusz <210165@edu.p.lodz.pl>
2019-03-12 09:12:10 +0100

    initial commit

---
+++ src/main/java/zzjp/App.java
@@ -0,0 +1,14 @@
+package zzjp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        TestClass testClass = new TestClass();
+
+        testClass.setText("blabla");
+        System.out.println(testClass.getText());
+    }
+}
